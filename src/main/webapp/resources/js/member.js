b3427e2e21eb753edffdfbdfdcca5ffc26a3ac89
yulyul2 <yulyul2o_o@naver.com>
2021-07-16 02:02:31 +0900

    resources upload

--- src/main/webapp/resources/js/member.js
+++ src/main/webapp/resources/js/member.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
 // DOM & css, image, js Loading
 $(window).load(function(){
-    
+
 });
 
 // login - form check
@@ -31,16 +31,14 @@ function joinFormCheck(){
         alert("아이디 값을 입력하세요.");
         form.id.focus();
         return false;
-    }else if(form.password.value == ""){
+    }
+    
+    if(form.password.value == ""){
         alert("비밀번호 값을 입력하세요.");
         form.password.focus();
         return false;
     }else if(form.passwordCheck.value == "" ){
         alert("비밀번호 확인 값을 입력하세요.");
-        form.passwordCheck.focus();
-        return false;
-    }else if(form.passwordCheck.value != form.password.value){
-        alert("비밀번호 값이 다릅니다. 다시 확인해 주세요.");
         form.passwordCheck.focus();
         return false;
     }else if(form.name.value == ""){
@@ -50,6 +48,44 @@ function joinFormCheck(){
     }else if(form.tel.value == ""){
         alert("휴대폰 번호 값을 입력하세요.");
         form.tel.focus();
+        return false;
+    }else if(form.email.value == ""){
+        alert("이메일 주소 값을 입력하세요.");
+        form.email.focus();
+        return false;
+    }else{
+        if(form.passwordCheck.value != form.password.value){
+            alert("비밀번호 값이 다릅니다. 다시 확인해 주세요.");
+            form.passwordCheck.focus();
+            return false;
+        }        
+        return true;
+    }
+    
+}
+
+// accountfind id - form check
+function findIdFormCheck(){
+    var form = document.findIdForm;
+    if(form.name.value == ""){
+        alert("이름 값을 입력하세요.");
+        form.name.focus();
+        return false;
+    }else if(form.email.value == ""){
+        alert("이메일 주소 값을 입력하세요.");
+        form.email.focus();
+        return false;
+    }else{
+        return true;
+    }
+}
+
+// accountfind id - form check
+function findPasswordFormCheck(){
+    var form = document.findPasswordForm;
+    if(form.id.value == ""){
+        alert("아이디 값을 입력하세요.");
+        form.id.focus();
         return false;
     }else if(form.email.value == ""){
         alert("이메일 주소 값을 입력하세요.");
