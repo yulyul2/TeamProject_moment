5eac6362a952bf3853e9a796baa61f064235e003
yulyul2 <yulyul2o_o@naver.com>
2021-07-17 18:53:33 +0900

    clear git cache

--- src/main/java/kr/co/controller/userController.java
+++ src/main/java/kr/co/controller/userController.java
@@ -3,14 +3,12 @@ package kr.co.controller;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import org.apache.ibatis.javassist.bytecode.stackmap.BasicBlock.Catch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,6 +16,7 @@ import kr.co.service.userService;
 import kr.co.vo.userVO;
 
 @Controller
+@RequestMapping("/user")
 public class userController {
 	private static final Logger logger = LoggerFactory.getLogger(postController.class);	
 	
@@ -25,12 +24,12 @@ public class userController {
 	userService service;
 	
 	//로그인 페이지 불러오기
-	@GetMapping("/user/login")
+	@GetMapping("/login")
 	public String getlogin() {
 	   return "user/login";
     }
 	
-	@PostMapping(value="/user/loginPro")							//@RequestParam 은 login.jsp 에서 member_pw값을 가져옴.
+	@PostMapping("/loginPro")							//@RequestParam 은 login.jsp 에서 member_pw값을 가져옴.
 	public String postlogin(userVO uservo, HttpServletRequest req,@RequestParam("member_pw") String member_pw) {
 		userVO vo = service.loginPro(uservo);
 		
@@ -59,45 +58,39 @@ public class userController {
 	}
 	
 	//회원가입 페이지 불러오기
-	@GetMapping("/user/join")
+	@GetMapping("/join")
 	public String getjoinuser() {
 		logger.info("get userJoin");
-		return "user/userJoin";
+		return "user/join";
 	}
 	
 	//회원가입 post
-	@PostMapping(value = "/user/join")
+	@PostMapping("/join")
 	public String joinMember(userVO uservo, RedirectAttributes rttr) throws Exception {
 		logger.info("post userJoin");
-		
 		service.userJoin(uservo);
-		
 		return "user/login";
 	}
 	
 	//ID,PW페이지 가져오기
-	@GetMapping("/user/searchIdPw")
+	@GetMapping("/searchId")
 	public String getsearchIdPw1() {
 		return "/user/accountFind";
 	}
 	
 	//ID찾기
-	@PostMapping("/user/searchId")
+	@PostMapping("/searchId")
 	public String searchId(userVO mvo, RedirectAttributes reat) {
 		userVO mVO = service.searchId(mvo);
-		
 		reat.addFlashAttribute("Id",mVO.getmember_id());
-		
 		return "user/accountFind";
 	}
 		
 	//PW찾기
-	@PostMapping("/user/searchPw")
+	@PostMapping("/searchPw")
 	public String searchPw(userVO mmvo, RedirectAttributes reat) {
 		userVO mmVO = service.searchPw(mmvo);
-			
 		reat.addFlashAttribute("Pw",mmVO.getmember_pw());
-		
 		return "user/accountFind";
 	}
 }
